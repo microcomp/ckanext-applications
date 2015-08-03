c3fcf022e58d8581c9b96cdb562da300acc6137e
János Farkas <farkas48@uniba.sk>
2015-08-03 15:24:48 +0200

    Update apps.py

--- ckanext/apps_and_ideas/apps.py
+++ ckanext/apps_and_ideas/apps.py
@@ -1179,7 +1179,9 @@ def is_admin():
                 'user': c.user or c.author, 'auth_user_obj': c.userobj,
                 'for_view': True}
     try:
-        _check_access('app_edit', context)
+        _check_access('app_edit', context, {'owner_id':""})
+        # if data_dict['owner_id']==user_id or user_has_role(user_id, Roles.MOD_R_APP)
+        # data_dict['owner_id']==user_id >> false
         return True
     except toolkit.NotAuthorized, e:
          return False         
